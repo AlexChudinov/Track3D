55ba4dbdb9256927bbbf7c3a65ef32c881fd9cb6
AlexChudinov <chudinov@inbox.ru>
2017-05-22 16:13:25 +0300

    Collision addition started

--- RandomProcess/RandomProcess.cpp
+++ RandomProcess/RandomProcess.cpp
@@ -8,6 +8,10 @@ RandomProcess::PProcess RandomProcess::createRandomProcess(const RandomProcessPa
 	switch (type) {
 	case DIFFUSION_VELOCITY_JUMP:
 		return PProcess(new DiffusionVelocityJump((const DiffusionParams &)params));
+	case DIFFUSION_COORD_JUMP:
+		return PProcess(new DiffusionCoordJump((const DiffusionParams&)params));
+	case COLLISION:
+		return PProcess(new Collision((const Collision::CollisionParams&)params));
 	default:
 		return PProcess();
 	}
@@ -27,21 +31,41 @@ const char* RandomProcess::rndProcName(RandomProcessType nType)
 RandomProcess::RandomProcess(RndGenResult seed)
 	:
 	m_generator(seed),
-	m_distribution(0.0, 1.0)
+	m_uniDistrib(0.0, 1.0),
+	m_normalDistrib(0.0, 1.0)
 {
 }
 
 double RandomProcess::rand()
 {
-	return m_distribution(m_generator);
+	return m_uniDistrib(m_generator);
+}
+
+double RandomProcess::randn()
+{
+	return m_normalDistrib(m_generator);
 }
 
 EP::Vector3D RandomProcess::randOnSphere()
 {
 	double phi = 2. * Const_PI * rand();
 	double thetta = acos(1 - 2 * rand());
-	return EP::Vector3D{ sin(phi)*sin(thetta), cos(phi)*sin(thetta), cos(thetta) };
+	return Vector3D{ sin(phi)*sin(thetta), cos(phi)*sin(thetta), cos(thetta) };
 }
+
+RandomProcess::Vector3D RandomProcess::rndMol(const Vector3D & vrel, double Tgas, double Mgas)
+{
+	Vector3D res;
+
+	double sigma = sqrt(Const_Boltzmann * Tgas / Mgas);
+
+	do {
+		res = sigma * Vector3D{ randn(), randn(), randn() };
+	} while (res.length() < 3.0 * sigma * rand());
+
+	return res;
+}
+
 
 DiffusionVelocityJump::DiffusionVelocityJump(const DiffusionParams & params)
 	:
@@ -50,6 +74,11 @@ DiffusionVelocityJump::DiffusionVelocityJump(const DiffusionParams & params)
 	m_fIonMobility(params.ionMobility),
 	m_fIonCharge(params.ionCharge)
 {
+}
+
+const char * DiffusionVelocityJump::rndProcName() const
+{
+	return _T("Velocity Jump");
 }
 
 DiffusionVelocityJump::Item DiffusionVelocityJump::randomJump(const Item & i1, const Item & i2)
@@ -75,12 +104,22 @@ DiffusionVelocityJump::Item DiffusionVelocityJump::randomJump(const Item & i1, c
 	return ret;
 }
 
+DiffusionVelocityJump::Item DiffusionVelocityJump::gasDependedRndJmp(const ItemNode & i1, const ItemNode & i2)
+{
+	return randomJump(i1.first, i2.first);
+}
+
 DiffusionCoordJump::DiffusionCoordJump(const DiffusionParams & params)
 	:
 	RandomProcess(params.seed),
 	m_fIonMobility(params.ionMobility),
 	m_fIonCharge(params.ionCharge)
 {
+}
+
+const char * DiffusionCoordJump::rndProcName() const
+{
+	return _T("Coordinates Jump");
 }
 
 DiffusionCoordJump::Item DiffusionCoordJump::randomJump(const Item & i1, const Item & i2)
@@ -105,4 +144,44 @@ DiffusionCoordJump::Item DiffusionCoordJump::randomJump(const Item & i1, const I
 
 	ret.pos += dr;
 	return ret;
+}
+
+DiffusionCoordJump::Item DiffusionCoordJump::gasDependedRndJmp(const ItemNode & i1, const ItemNode & i2)
+{
+	return randomJump(i1.first, i2.first);
+}
+
+Collision::Collision(const CollisionParams & params)
+	:
+	RandomProcess(params.seed),
+	m_fIonCrossSection(params.ionCrossSection),
+	m_fIonMass(params.ionMass),
+	m_fGasMass(params.gasMass)
+{
+}
+
+const char * Collision::rndProcName() const
+{
+	return _T("Collisions");
+}
+
+Collision::Item Collision::gasDependedRndJmp(const ItemNode & in1, const ItemNode & in2)
+{
+	Item res = in2.first;
+	double h = in2.first.time - in1.first.time;
+	double Tgas = (in1.second.temp + in2.second.temp) / 2.;
+	Vector3D velIonRel = ((in1.first.vel - in1.second.vel) + (in2.first.vel - in2.second.vel)) / 2.;
+	
+	//if(meanRelativeSpeed(velIonRel, Tgas) * m_fIonCrossSection)
+
+	return res;
+}
+
+double Collision::meanRelativeSpeed(const Vector3D & velIonRel, double Tgas) const
+{
+	double
+		A = m_fGasMass / 2. / Const_Boltzmann / Tgas,
+		meanGasSpeed = 2 / sqrt(A * Const_PI),
+		s = velIonRel.length() * sqrt(A);
+	return meanGasSpeed*((s + 1. / (2.*s))*sqrt(Const_PI) / 2.*EP::CMath::erf(s) + 1. / 2. * exp(-s*s));
 }
