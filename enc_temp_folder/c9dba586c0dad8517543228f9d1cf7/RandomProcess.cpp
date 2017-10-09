2c4fae566f27a63ecc668b885befc2cdf6d1ae21
AlexChudinov <chudinov@inbox.ru>
2017-10-09 12:26:59 +0300

    Last fix

--- enc_temp_folder/c9dba586c0dad8517543228f9d1cf7/RandomProcess.cpp
+++ enc_temp_folder/c9dba586c0dad8517543228f9d1cf7/RandomProcess.cpp
@@ -59,11 +59,11 @@ RandomProcess::Vector3D RandomProcess::rndMol(const Vector3D & vrel, double Tgas
 	Vector3D res;
 
 	double sigma = sqrt(Const_Boltzmann * Tgas / Mgas);
-	double vbig = sqrt((vrel & vrel) + 9.0 * sigma * sigma) * rand();
+	double vbig = sqrt((vrel & vrel) + 9.0 * sigma * sigma);
 
 	do {
 		res = sigma * Vector3D{ randn(), randn(), randn() };
-	} while ((res - vrel).length() < vbig);
+	} while ((res - vrel).length() < vbig * rand());
 
 	return res;
 }
